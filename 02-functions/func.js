81d5c346ec6e4f673278ba6c3e02536116668557
kunxin-chor <chorkunxin@yahoo.com>
2022-09-29 06:43:01 +0000

    Added function example

---
+++ 02-functions/func.js
@@ -0,0 +1,22 @@
+// correct ans
+// function multi(x) {
+//     return x * 2;
+// }
+
+// let ans = multi(2);
+// console.log(ans);
+
+// to write a function
+// 1. you must know what the result of the function to be
+// 2. you must know the incoming parameters (aka input)
+// 2b the variable name doesn't matter (it must be a valid name to begin with)
+// 2c if there is more than one input (i.e incoming parameters), it's the ORDER
+//    that matters
+// 3. you must know what the results should be
+// 4. you must know how to calculate the results from the input
+function multi(x) {
+    // if we are stuck on how to solve something
+    // always begin with asking "do I need a new variable to store the result or some interimedate step?"
+    let result = x * 2;
+    return result;
+}
